8cc9a4d68ae8198637891f53f88ee3215cec8d3e
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-20 06:23:50 +0000

    Correct case sensitive key bindings, implement key bindings for cheeky 'Say___' messages, test snake runs smoothly

--- input.js
+++ input.js
@@ -6,27 +6,47 @@ const handleUserInput = function(key) {
       process.exit();
     }
 
-  if (key === 'W') {
+  if (key === 'w') {
   connection.write('Move: up');
-  console.log('up');
   } 
   
-  if (key === 'A') {
+  if (key === 'a') {
     connection.write('Move: left');
-    console.log('left')
   } 
 
-  if (key ==='S') {
+  if (key ==='s') {
   connection.write('Move: down')
-  console.log('down');
   } 
   
-  if (key === 'D') {
+  if (key === 'd') {
   connection.write('Move: right');
-  console.log('right');
   }
 
- }
+  if (key === '1') {
+    connection.write('Say: I can\'t be defeated!');
+  }
+
+  if (key === '2') {
+    connection.write('Say: Nomnomnom');
+  }
+
+  if (key === '3') {
+    connection.write('Say: SsSssSs');
+  }
+
+  if (key === '4') {
+    connection.write('Say: LMAO');
+  }
+
+  if (key === '5') {
+    connection.write('Say: gg');
+  }
+
+  if (key === '6') {
+    connection.write('Say: HAHA you died');
+  }
+
+ };
 
 
  const setupInput = function(conn) {
@@ -37,7 +57,7 @@ const handleUserInput = function(key) {
   stdin.on('data', handleUserInput);
   stdin.resume();
   return stdin;
-}
+};
 
 
 module.exports = { setupInput };
