f61ff0c615e3baebd52ba1800544ca21d5add98a
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-21 03:19:20 +0000

    Clean up comments for project submission

--- client.js
+++ client.js
@@ -1,14 +1,14 @@
 const net = require('net');
 const { IP, PORT } = require('./constants');
 
-//Establishes connection with the game server
+// establishes connection with the game server
 const connect = function() {
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
 
-  // interprets incoming data as text
+  // interpret incoming data as text
   conn.setEncoding('utf8');
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
