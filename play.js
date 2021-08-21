e949a34804cdf0f18b142524e150cfca09441c09
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-21 02:51:30 +0000

    Remove redundant connect function which causes connection message to print twice

--- play.js
+++ play.js
@@ -1,10 +1,5 @@
 const { connect } = require('./client');
 const { setupInput } = require('./input');
-
-//connect to server
 console.log('Connecting ...');
-connect();
-
-//handle user input
 const conn = connect();
 setupInput(conn);
