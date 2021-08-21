1d6fa44920cc572baf48af80d73a3b25cfe023ab
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-21 02:52:44 +0000

    Add required constants file functions to client file

--- client.js
+++ client.js
@@ -1,21 +1,19 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 
 //Establishes connection with the game server
 
 const connect = function() {
-  const conn= net.createConnection({ 
-    host: 'localhost', //IP address here
-    port: 50541 //PORT number here
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
   });
+
   // interpret incoming data as text
   conn.setEncoding('utf8');
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write('Name: RAY');
-    // setInterval(() => {
-    //   conn.write('Move: up');
-    // }, 50);
-    
   });
 
   conn.on('data', (data) => {
@@ -24,9 +22,6 @@ const connect = function() {
   
   return conn;
 };
-
-// console.log('Connecting ...');
-// connect();
 
 //EXPORT
 module.exports = { connect };
